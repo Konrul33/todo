7864eceff8133d71fe10a4f638c2b8705821803c
konrul33 <konrul33@hotmail.com>
2023-07-23 12:16:52 +0300

    todolist

--- js.js
+++ js.js
@@ -1,5 +1,3 @@
-
-// Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -7,14 +5,13 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Bir şey yazmalısınız");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
